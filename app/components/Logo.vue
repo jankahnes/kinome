2cfe967b4af7f71f05387873036eee68c056f00d
jankahnes <jan.kahnes2@gmail.com>
2025-11-22 22:42:53 +0100

    redesign + nuxt4 migration

---
+++ app/components/Logo.vue
@@ -0,0 +1,21 @@
+<template>
+  <NuxtLink to="/" class="">
+    <div class="flex items-center">
+      <h1 class="text-5xl font-bold primary-gradient gradient-clip tracking-tighter z-10 gradient-text">
+        kinome
+      </h1>
+    </div>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.gradient-text {
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
+  background-clip: text;
+  color: transparent;
+}
+</style>
